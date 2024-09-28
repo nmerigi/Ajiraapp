eeaa355e0b6c58ace137a138ad628e5c88a66ac5
Nicole Merigi <nmerigi@gmail.com>
2024-09-28 23:49:35 +0300

    Made the signup and log in front end

---
+++ app/src/main/java/com/example/ajiraapp/HelperClass.java
@@ -0,0 +1,4 @@
+package com.example.ajiraapp;
+
+public class HelperClass {
+}
--- app/src/main/java/com/example/ajiraapp/LogIn.java
+++ app/src/main/java/com/example/ajiraapp/LogIn.java
@@ -8,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-public class MainActivity extends AppCompatActivity {
+public class LogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        FirebaseApp.initializeApp(this);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
     }
 }
